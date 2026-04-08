5960ebb5f4d12d82c89c42791e022d12626cef82
StivoMix <139881217+StivoMix@users.noreply.github.com>
2026-04-09 00:21:52 +0300

    Refactor and enhance orb management functions

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 async function sha256(text) {
-    const encoder = new TextEncoder();
+    const encoder = new TextEncoder(); //test
     const data = encoder.encode(text);
     const hashBuffer = await crypto.subtle.digest("SHA-256", data);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
